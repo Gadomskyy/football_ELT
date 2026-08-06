616492acd9647153b9bbf546c44c59b37f011bda
Gadomskyy <118121980+Gadomskyy@users.noreply.github.com>
2026-08-06 21:46:07 +0200

    create DAG extracting PL teams for last 3 seasons

---
+++ airflow/dags/premier_league_teams_extract.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+import os
+from datetime import datetime, timezone
+from airflow.sdk import dag, task
+from src.common.api_to_bigquery import extract_flatten_and_load
+from src.common.commons import GCP_PROJECT_ID, GCP_LOCATION
+
+#VARIABLES
+SEASONS = [2024, 2025, 2026]
+
+API_URL = (
+    "https://api.football-data.org/v4/competitions/PL/teams"
+)
+
+@dag(
+    dag_id="football_data_pl_teams_bronze",
+    description=(
+        """
+        Loads Premier League teams from the football-data.org API into a BigQuery table in the bronze layer.
+        Contains data for the seasons: 2024/2025, 2025/2026, and 2026/2027.       
+        """
+    ),
+    schedule="@monthly",
+    start_date=datetime(2026,1,1,tzinfo=timezone.utc),
+    catchup=False,
+    max_active_runs=1,
+    tags=[
+        "football-data",
+        "premier-league",
+        "bronze"
+    ]
+)
+
+def football_data_pl_teams_bronze():
+
+    @task(task_id="load_pl_teams_to_bronze")
+    def load_teams(season:int) -> dict:
+        api_key = os.getenv(
+                "FOOTBALL_DATA_API_KEY"
+        )
+
+        if not api_key:
+            raise RuntimeError(
+                "No FOOTBALL_DATA_API_KEY."
+            )
+
+        season_end = str(season + 1)[-2:]
+
+        table_name = (
+            f"football_data_pl_teams_"
+            f"{season}_{season_end}"
+        )
+
+        return extract_flatten_and_load(
+            api_url=API_URL,
+            records_key='teams',
+            project_id=GCP_PROJECT_ID,
+            dataset_id='bronze',
+            table_name=table_name,
+            headers= {
+                'X-auth-token': api_key
+            },
+            params={
+                "season": season,
+            },
+            location=GCP_LOCATION,
+            write_disposition= "WRITE_TRUNCATE"
+        )
+
+    load_teams.expand(season=SEASONS)
+
+dag = football_data_pl_teams_bronze()
