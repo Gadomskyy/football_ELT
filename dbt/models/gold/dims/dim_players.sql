e805fa27a0b685aeadf91d2e553c3b2ccda408fb
Gadomskyy <118121980+Gadomskyy@users.noreply.github.com>
2026-08-16 22:27:31 +0200

    update dim_players

--- dbt/models/gold/dims/dim_players.sql
+++ dbt/models/gold/dims/dim_players.sql
@@ -1,15 +1,48 @@
 WITH base_players AS (
-    SELECT
+    SELECT 
     *
     FROM {{ ref('pl_squads') }}
+
+),
+
+latest_player_record AS (
+
+    SELECT
+        player_id,
+        player_name,
+        player_position,
+        player_dob,
+        player_nationality,
+        season,
+        ROW_NUMBER() OVER (
+            PARTITION BY player_id
+            ORDER BY season DESC
+        ) AS row_num
+
+    FROM base_players
+
+),
+
+player_seasons AS (
+
+    SELECT
+        player_id,
+        MIN(season) AS first_season,
+        MAX(season) AS last_season
+    FROM base_players
+    GROUP BY player_id
+
 )
 
 SELECT
-    player_id,
-    player_name,
-    player_position,
-    player_dob,
-    player_nationality,
-    MAX(season) AS last_season
-FROM base_players
-GROUP BY 1,2,3,4,5
+    p.player_id,
+    p.player_name,
+    p.player_position AS latest_position,
+    p.player_dob,
+    p.player_nationality,
+    s.first_season,
+    s.last_season
+FROM latest_player_record p
+LEFT JOIN player_seasons s
+    ON p.player_id = s.player_id
+WHERE p.row_num = 1
